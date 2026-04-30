6e08060bf42d06ccb61cc699d67e713324d81053
BlackHeart0731 <black.haert.0731@gmail.com>
2026-04-30 17:46:21 +0900

    Fix Vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/ink-canvas/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
